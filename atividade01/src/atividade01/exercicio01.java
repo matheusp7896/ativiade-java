bbe5a27b28d4b72353c3d5bd146d4189cb979fcc
matheusp7896 <115594800+matheusp7896@users.noreply.github.com>
2023-07-30 23:20:36 -0300

    Add files via upload

---
+++ atividade01/src/atividade01/exercicio01.java
@@ -0,0 +1,13 @@
+package atividade01;
+
+public class exercicio01 {
+
+	public static void main(String[] args) {
+     
+		int valor01 = 10; 
+		int valor02 =  25;
+		
+		System.out.println(valor01 + valor02);
+	}
+
+}
---
+++ atividade01/src/atividade01/exercicio02.java
@@ -0,0 +1,13 @@
+package atividade01;
+
+public class exercicio02 {
+
+	public static void main(String[] args) {
+		double raio = 60;
+		
+		double area = 3.14159 * (raio * 2);
+		
+		System.out.println(area);
+	}
+
+}
